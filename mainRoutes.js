3b02afc970f69fe2da54c920c79c9acd5e6c6f6c
Tumisho <tumisomphela020304@gmail.com>
2025-05-01 12:25:25 +0200

    feat(elements): Create and join button UI

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,11 +2,11 @@ const path = require('path')
 const express = require('express')
 const mainRouter = express.Router()
 
-mainRouter.get('/', function (req, res) {
-    res.send('Hello World')
-})
+//mainRouter.get('/', function (req, res) {
+//    res.send('Hello World')
+//})
 
-mainRouter.get('/index', function (req, res) {
+mainRouter.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
