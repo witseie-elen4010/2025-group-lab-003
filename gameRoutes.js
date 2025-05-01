e361a76de9000879a6a6e3f7bb5ca165c8b14919
Okwukwe Mbajiorgu <2430639@students.wits.ac.za>
2025-05-01 22:54:48 +0200

    feat(router): create API routes calling game logic

---
+++ gameRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+// Import gameController from the backend folder
+const gameController = require('./backend/gameController');
+
+const router = express.Router();
+
+router.post('/create', gameController.createGame);
+router.post('/join', gameController.joinGame);
+router.post('/start', gameController.startGame);
+router.post('/vote', gameController.voteOut);
+
+module.exports = router;
