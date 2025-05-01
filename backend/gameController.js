055b5cbd8e3ebfcf34d8532964c1b65c5b131104
Okwukwe Mbajiorgu <2430639@students.wits.ac.za>
2025-05-01 23:22:24 +0200

    feat(core): made changes to index and mainRoutes, created backend + public folder

---
+++ backend/gameController.js
@@ -0,0 +1,15 @@
+// THIS FILE IS FOR GAME LOGIC
+const games = {}; // This will store the games in memory
+
+// Create Game
+router.createGame = (req, res) => {
+  // TODO: Implement game creation logic
+}
+
+// Join Game
+router.joinGame = (req, res) => {
+  // TODO: Implement join game logic
+}
+
+
+module.exports = router
--- mainRoutes.js
+++ mainRoutes.js
@@ -2,9 +2,9 @@ const path = require('path')
 const express = require('express')
 const mainRouter = express.Router()
 
-//mainRouter.get('/', function (req, res) {
-//    res.send('Hello World')
-//})
+// Serve static files from the "public" directory
+mainRouter.use(express.static(path.join(__dirname, 'public')));
+
 
 mainRouter.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
