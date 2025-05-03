707d42048bef79ad0c5c7244213d9105e1c34f3c
Jaimedacruz <jaimerobertodacruzjunior@gmail.com>
2025-05-03 21:07:01 +0200

    feat(elements): creategame function

--- backend/gameController.js
+++ backend/gameController.js
@@ -3,8 +3,16 @@ const games = {}; // This will store the games in memory
 
 // Create Game
 exports.createGame = (req, res) => {
-  // TODO: Implement game creation logic
-}
+  const gameCode = Math.random().toString(36).substring(2, 8).toUpperCase();
+  const { creatorName } = req.body;
+  games[gameCode] = {
+    creatorName,
+    players: [{ name: creatorName, role: 'creator' }],
+    status: 'waiting', // waiting, in-progress, completed
+  };
+  res.json({ code: gameCode });
+};
+
 
 // Join Game
 exports.joinGame = (req, res) => {
