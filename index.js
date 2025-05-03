60483b2c930561880a0477c242dc4f0eb620be09
Jaimedacruz <jaimerobertodacruzjunior@gmail.com>
2025-05-03 21:36:21 +0200

    feat(elements): uncommented code for API connection

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict'
 const express = require('express');
 const path = require('path');
-// (PROVIDED THERE IS NO GAME CONTROLLER FILE) const gameRoutes = require('./gameRoutes');  // Import game routes
+const gameRoutes = require('./gameRoutes');  // Import game routes
 
 const app = express();
 
 const mainRouter = require('./mainRoutes')
 
-// (PROVIDED THERE IS NO GAME CONTROLLER FILE) app.use(express.json()); // parse incoming JSON payloads
-// (PROVIDED THERE IS NO GAME CONTROLLER FILE) app.use(express.urlencoded({ extended: true })); // parse URL-encoded form data (i.e., data sent from HTML forms)
+app.use(express.json()); // parse incoming JSON payloads
+app.use(express.urlencoded({ extended: true })); // parse URL-encoded form data (i.e., data sent from HTML forms)
 
 // Serve static files (like frontend.js) from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(mainRouter)
-// (PROVIDED THERE IS NO GAME CONTROLLER FILE) app.use('/api/game', gameRoutes); // Use the game routes for API calls
+app.use('/api/game', gameRoutes); // Use the game routes for API calls
 
 const port = process.env.PORT || 3000
 app.listen(port)
