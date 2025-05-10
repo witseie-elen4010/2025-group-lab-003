5121c17c1c4f234a1e8e4e907afc0c9bc010caa5
Andile Dlongwana <andydlongster@gmail.com>
2025-05-10 12:03:43 +0200

    test: adding tests to create game functionality

--- src/models/gameModels.js
+++ src/models/gameModels.js
@@ -1,42 +1,43 @@
-const db = require('../config/db');
-
 // Generate a 6-char game code
 const generateCode = () => Math.random().toString(36).substring(2, 8).toUpperCase();
 
 exports.createGame = async () => {
-  const gameCode = generateCode();
-  const query = `
-    INSERT INTO GameState (gameCode, round, gameStarted, winner, log)
-    VALUES (@gameCode, 1, 0, NULL, '[]')
-  `;
+    const db = require('../config/db');
+    const gameCode = generateCode();
+    const query = `
+        INSERT INTO GameState (gameCode, round, gameStarted, winner, log)
+        VALUES (@gameCode, 1, 0, NULL, '[]')
+    `;
 
-  const pool = await db.poolPromise;
-  await pool.request()
-    .input('gameCode', db.sql.VarChar, gameCode)
-    .query(query);
+    const pool = await db.poolPromise;
+    await pool.request()
+        .input('gameCode', db.sql.VarChar, gameCode)
+        .query(query);
 
-  return gameCode;
+    return gameCode;
 };
 
 exports.joinGame = async (playerName, gameCode) => {
-  const pool = await db.poolPromise;
+    const db = require('../config/db');
+    const pool = await db.poolPromise;
 
-  const query = `
-    INSERT INTO Players (userId, gameCode, role, word, status)
-    VALUES (@playerName, @gameCode, NULL, NULL, 'active')
-  `;
+    const query = `
+        INSERT INTO Players (userId, gameCode, role, word, status)
+        VALUES (@playerName, @gameCode, NULL, NULL, 'active')
+    `;
 
-  await pool.request()
-    .input('playerName', db.sql.VarChar, playerName) // used as userId
-    .input('gameCode', db.sql.VarChar, gameCode)
-    .query(query);
+    await pool.request()
+        .input('playerName', db.sql.VarChar, playerName) // used as userId
+        .input('gameCode', db.sql.VarChar, gameCode)
+        .query(query);
 };
 
 exports.getPlayersByGameCode = async (gameCode) => {
+    const db = require('../config/db');
     const pool = await db.poolPromise;
     const result = await pool.request()
-      .input('gameCode', db.sql.VarChar, gameCode)
-      .query(`SELECT userId FROM Players WHERE gameCode = @gameCode`);
+        .input('gameCode', db.sql.VarChar, gameCode)
+        .query(`SELECT userId FROM Players WHERE gameCode = @gameCode`);
     return result.recordset;
 };
   
