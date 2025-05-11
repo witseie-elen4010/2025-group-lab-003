70724e8269d7ebec7d47e1de61e3309ac534dd9d
Jaimedacruz <jaimerobertodacruzjunior@gmail.com>
2025-05-11 16:15:57 +0200

    test: adding test to join lobby functionality

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -4,6 +4,7 @@ const gameController = require('../controllers/gameController');
 const router = express.Router();
 
 router.post('/create', gameController.createGame);
+router.post('/join', gameController.joinGame);
 router.get('/players/:gameCode', gameController.getPlayers); 
 
 module.exports = router;
